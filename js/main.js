6218e84fdc04b5a5e1e414e22c60871d493a5a79
Инна Русина <rus.work89@yandex.ru>
2021-09-28 22:07:53 +0300

    Добавлены 2 функции
    1 функция выдает случайное число;
    2 функция сравнивает длину строки с максимальным значением.

---
+++ js/main.js
@@ -0,0 +1,15 @@
+function getRandomNumber (minNumber, maxNumber) {
+    if (minNumber >= 0 && maxNumber >= 0 && minNumber < maxNumber) {
+        randomNumber = Math.random() * (maxNumber-minNumber) + minNumber;
+        return Math.floor(randomNumber);
+    }
+    return 'Диапазон должен состоять только из положительных чисел, и минимальное число должно быть меньше максимального';
+}
+
+getRandomNumber(0, 10);    // временная строка, чтобы ESLint не ругался
+
+function getLengthString (string, maxLength) {
+    return (string.length <= maxLength) ? true : false;
+}
+
+getLengthString('Привет', 6);    // временная строка, чтобы ESLint не ругался
