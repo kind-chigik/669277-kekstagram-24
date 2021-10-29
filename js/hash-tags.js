0997ad14d4ea7c82b8caf22bd4b9277c177530f8
Инна Русина <rus.work89@yandex.ru>
2021-10-30 02:31:22 +0300

    Внесены корректировки:
    В гит попала не актуальная версия кода. В текущей версии:
    1. Добавлен обработчик события ввода
    2. Удален массив для невалидных сообщений, как лишнее звено
    3. Удалена функция checkHashtags, т.к. была лишней.

--- js/hash-tags.js
+++ js/hash-tags.js
@@ -4,52 +4,49 @@ const MAX_LENGTH_HASHTAG = 20;
 const MAX_HASHTAGS = 5;
 
 
-const checkHashtags = (text) => {
+buttonHashtags.addEventListener('input', () => {
+  const text = buttonHashtags.value;
   buttonHashtags.setCustomValidity('');
-  const invalidMessage = [];
   const hashtags = text.toLowerCase().trim();
   const arrayHashtags = hashtags.split(/\s+/);
   if (arrayHashtags.length === 0) {
     return;
   }
-
   const isStartNoHashtag = arrayHashtags.some((item) => item[0] !== '#');
   if (isStartNoHashtag) {
-    invalidMessage.push('Хэш-тег должен начинаться с #');
+    buttonHashtags.setCustomValidity('Хэш-тег должен начинаться с #');
   }
 
   const isOnlyLattice = arrayHashtags.some((item) => item === '#');
   if (isOnlyLattice) {
-    invalidMessage.push('Хэш-тег не должен состоять только из #');
+    buttonHashtags.setCustomValidity('Хэш-тег не должен состоять только из #');
   }
 
   const isSpaceBetweenHashtags = arrayHashtags.some((item) => item.indexOf('#', 1) >= 1);
   if (isSpaceBetweenHashtags) {
-    invalidMessage.push('Хэш-теги должны разделяться пробелами');
+    buttonHashtags.setCustomValidity('Хэш-теги должны разделяться пробелами');
   }
 
   const isHastagsrepeat = arrayHashtags.some((item, i, arr) => arr.indexOf(item, i + 1) >= i + 1);
   if (isHastagsrepeat) {
-    invalidMessage.push('Хэш-теги не должны повторяться');
+    buttonHashtags.setCustomValidity('Хэш-теги не должны повторяться');
   }
 
   const isHashtagTooLong = arrayHashtags.some((item) => item.length > MAX_LENGTH_HASHTAG);
   if (isHashtagTooLong) {
-    invalidMessage.push('Хэш-теги не должны быть больше 20 символов');
+    buttonHashtags.setCustomValidity('Хэш-теги не должны быть больше 20 символов');
   }
 
   if (arrayHashtags.length > MAX_HASHTAGS) {
-    invalidMessage.push('Хэш-тегов не должно быть больше 5');
+    buttonHashtags.setCustomValidity('Хэш-тегов не должно быть больше 5');
   }
 
   arrayHashtags.forEach((element) => {
     if (!reForHashtags.test(element) && !isStartNoHashtag && !isOnlyLattice) {
-      invalidMessage.push('Хэш-тег должен содержать только буквы и цифры');
+      buttonHashtags.setCustomValidity('Хэш-тег должен содержать только буквы и цифры');
     }
   });
-
   buttonHashtags.reportValidity();
-  return buttonHashtags.setCustomValidity(invalidMessage);
-};
+});
 
-export {checkHashtags};
+export {buttonHashtags};
