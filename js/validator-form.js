ab6b3a8bc0fb137ba18f315d3014392685e56e56
Инна Русина <rus.work89@yandex.ru>
2021-10-30 02:27:37 +0300

    Внесены корректировки:
    В код попала не актуальная версия кода. В текущей версии:
    1. Функции исправлены на стрелочные
    2. Обработчик события для хеш-тегов перенесен в модуль хэш-тегов

--- js/validator-form.js
+++ js/validator-form.js
@@ -1,8 +1,7 @@
-import {checkHashtags} from './hash-tags.js';
+import {buttonHashtags} from './hash-tags.js';
 import {comments} from './comments.js';
 
 const formImageUpload = document.querySelector('.img-upload__form');
-const buttonHashtags = formImageUpload.querySelector('.text__hashtags');
 const buttonUpload = formImageUpload.querySelector('#upload-file');
 const formEditImage = formImageUpload.querySelector('.img-upload__overlay');
 const buttonClose = formImageUpload.querySelector('#upload-cancel');
@@ -11,36 +10,33 @@ const closeWindowTunning = () => {
   formEditImage.classList.add('hidden');
   document.body.classList.remove('modal-open');
   buttonUpload.value = '';
-  document.removeEventListener('keydown', onWindowKeydownEsc);
-  buttonClose.removeEventListener('click', onButtonCLoseClick);
 };
 
-function onWindowKeydownEsc (evt) {
+const onWindowKeydownEsc = (evt) => {
   if (evt.key === 'Escape' || evt.key === 'Esc') {
     evt.preventDefault();
     if (buttonHashtags === document.activeElement || comments === document.activeElement) {
       return;
     }
     closeWindowTunning();
+    document.removeEventListener('keydown', onWindowKeydownEsc);
   }
-}
+};
 
-function onButtonCLoseClick () {
+const onButtonCLoseClick = () => {
   closeWindowTunning();
-}
+  buttonClose.removeEventListener('click', onButtonCLoseClick);
+};
 
-function openWindowTunning () {
+const openWindowTunning = () => {
   formEditImage.classList.remove('hidden');
   document.body.classList.add('modal-open');
 
   document.addEventListener('keydown', onWindowKeydownEsc);
   buttonClose.addEventListener('click', onButtonCLoseClick);
-  buttonHashtags.addEventListener('input', () => {
-    const text = buttonHashtags.value;
-    checkHashtags(text);
-  });
-}
+};
 
 buttonUpload.addEventListener('change', () => {
   openWindowTunning();
 });
+
