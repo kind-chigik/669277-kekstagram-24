5a4e8bb35fe77b820f6493d7d1849995fb8aed77
Инна Русина <rus.work89@yandex.ru>
2021-11-02 21:10:02 +0300

    Скорректировано сообщение при ошибке

--- js/comments.js
+++ js/comments.js
@@ -1,9 +1,10 @@
 const comments = document.querySelector('.text__description');
+const MAX_CHAR_COMMENT = 140;
 
 comments.addEventListener('input', () => {
   const text = comments.value;
-  if (text.length > 140) {
-    comments.setCustomValidity('Сообщение не может быть больше 140 символов');
+  if (text.length > MAX_CHAR_COMMENT) {
+    comments.setCustomValidity('Комментарий не может быть больше 140 символов');
   } else {
     comments.setCustomValidity('');
   }
