a844d0dc7125e9738aa30a0ed7474477901ee45c
Инна Русина <rus.work89@yandex.ru>
2021-11-08 19:12:57 +0300

    Добавлен модуль для соединения с сервером

---
+++ js/loader.js
@@ -0,0 +1,22 @@
+const Urls = {
+  GET: 'https://24.javascript.pages.academy/kekstagram/data',
+  POST: 'https://24.javascript.pages.academy/kekstagram',
+};
+
+const request = (onSuccess, onError, method, body) => {
+  fetch(
+    Urls[method],
+    {
+      method: method,
+      body: body,
+    })
+    .then((response) => response.json())
+    .then((data) => {
+      onSuccess(data);
+    })
+    .catch((err) => {
+      onError(err);
+    });
+};
+
+export {request};
