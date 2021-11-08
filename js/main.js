b80426179092eca999b119053fb20d0db2462186
Инна Русина <rus.work89@yandex.ru>
2021-11-08 19:14:27 +0300

    Добавлен модуль соединения с сервером для получения изображений

--- js/main.js
+++ js/main.js
@@ -1,5 +1,30 @@
 import './data.js';
 import {renderPhotos} from './pictures-users/thumbnails.js';
 import './load-new-picture/validator-form.js';
+import {request} from './loader.js';
+import {createBlockMessage, addBlockMessage} from './helper.js';
 
-renderPhotos();
+const templateError = document.querySelector('#error').content;
+const error = templateError.querySelector('.error');
+const ERROR_MESSAGE = 'Ошибка загрузки изображений';
+const ERROR_TEXT_BUTTON = 'Ужасно';
+
+let photos = [];
+
+const errorLoadImages = createBlockMessage(error, 'error-load-images');
+const errorLoadMessage = errorLoadImages.querySelector('.error__title');
+const errorLoadButton = errorLoadImages.querySelector('.error__button');
+errorLoadMessage.textContent = ERROR_MESSAGE;
+errorLoadButton.textContent = ERROR_TEXT_BUTTON;
+
+
+const onSuccess = (data) => {
+  photos = data.slice();
+  renderPhotos(photos);
+};
+
+const onError = () => {
+  addBlockMessage(errorLoadImages);
+};
+
+request(onSuccess, onError, 'GET');
