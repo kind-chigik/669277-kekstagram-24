c171a8d9c64537ac6ed9492e2af19c6e721ba2ee
Инна Русина <rus.work89@yandex.ru>
2021-11-10 21:48:26 +0300

    Добавлена сортировка фотографий

--- js/pictures-users/thumbnails.js
+++ js/pictures-users/thumbnails.js
@@ -1,10 +1,42 @@
 import {showBigPicture} from '../pictures-users/big-picture.js';
+import {shuffleArray} from '../helper.js';
 
 const templatePicture = document.querySelector('#picture').content;
 const picture = templatePicture.querySelector('.picture');
 const pictures = document.querySelector('.pictures');
 
+const filterPicture = document.querySelector('.img-filters__form');
+const MAX_RANDOM_PHOTO = 10;
+const VALUE_FROM = 0;
 const fragment = document.createDocumentFragment();
+
+const addActiveClass = (evt) => {
+  const activeElement = filterPicture.querySelector('.img-filters__button--active');
+  activeElement.classList.remove('img-filters__button--active');
+  evt.target.classList.add('img-filters__button--active');
+};
+
+const compareElement = (element1, element2) => element2.comments.length - element1.comments.length;
+
+const sortPictures = (photos, render) => {
+  const clonePhotos = photos.slice();
+  filterPicture.addEventListener('click', (evt) => {
+    if (evt.target.closest('#filter-random')) {
+      addActiveClass(evt);
+      const randomPhotos = shuffleArray(clonePhotos).slice(VALUE_FROM, MAX_RANDOM_PHOTO);
+      render(randomPhotos);
+    }
+    if (evt.target.closest('#filter-discussed')) {
+      addActiveClass(evt);
+      clonePhotos.sort(compareElement);
+      render(clonePhotos);
+    }
+    if (evt.target.closest('#filter-default')) {
+      addActiveClass(evt);
+      render(photos);
+    }
+  });
+};
 
 const renderPhoto = (data) => {
   const newPicture = picture.cloneNode(true);
@@ -25,8 +57,13 @@ const renderPhotos = (arrayPhotos) => {
   arrayPhotos.forEach((element) => {
     fragment.appendChild(renderPhoto(element));
   });
-
+  const allPistures = pictures.querySelectorAll('.picture');
+  if (allPistures.length > 0) {
+    for (let i = 0; i < allPistures.length; i++) {
+      pictures.removeChild(allPistures[i]);
+    }
+  }
   pictures.appendChild(fragment);
 };
 
-export {renderPhotos};
+export {renderPhotos, sortPictures};
