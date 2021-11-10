5c2d14938d35236522a328e85ecc47ceee205c74
Инна Русина <rus.work89@yandex.ru>
2021-11-10 21:49:02 +0300

    Добавлена функция для перемешивания элементов массива

--- js/helper.js
+++ js/helper.js
@@ -6,13 +6,23 @@ export const getRandomNumber = (minNumber, maxNumber) => {
   return -1;
 };
 
+export const shuffleArray = (array) => {
+  for (let i = array.length-1; i > 0; i--) {
+    const randomNumber = getRandomNumber(0, array.length);
+    const randomElement = array[randomNumber];
+    array[randomNumber] = array[i];
+    array[i] = randomElement;
+  }
+  return array;
+};
+
 export const getRandomArrayElement = (elements) => elements[getRandomNumber(0, elements.length - 1)];
 
 export const checkLengthString = (string, maxLength) =>(string.length <= maxLength);
 
 export const createBlockMessage = (elementForClone, id) => {
   const blockMessage = elementForClone.cloneNode(true);
-  blockMessage.setAttribute('id', id);
+  blockMessage.id = id;
   blockMessage.classList.add('hidden');
   document.body.appendChild(blockMessage);
   return blockMessage;
@@ -29,3 +39,4 @@ export const addBlockMessage = (element) => {
     }
   });
 };
+
