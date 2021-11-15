70cccfde6797401b5cd7da3de55cb955a946ea0c
Инна Русина <rus.work89@yandex.ru>
2021-11-15 19:59:49 +0300

    Удалены комментарии

--- js/utils/debounce.js
+++ js/utils/debounce.js
@@ -1,6 +1,3 @@
-// Функция взята из интернета и доработана
-// Источник - https://www.freecodecamp.org/news/javascript-debounce-example
-
 const DELAY = 500;
 
 const debounce = (callback, timeoutDelay = DELAY) => {
